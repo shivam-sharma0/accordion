2dea0bcba96b3e444c40d6b43ca036797e556e83
shivam-sharma0 <vashistshivam931@gmail.com>
2023-04-24 11:09:56 +0530

    second

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [questions,setQuestions]=useState(data);
   return <main>
     <div className="container">
-      <h3>Questions and answer about login</h3>
+      <h3>Questions and answers about login</h3>
       <section className='info'>
         {questions.map((question)=>{
           return <SingleQuestion key={question.id} {...question}/>
